ac4bb163d052103631d767b433c10113a3a466d0
rshipley160 <42752250+rshipley160@users.noreply.github.com>
2021-10-20 08:56:03 -0400

    renamed transfer_size to numElements for clarity

--- Performance Experiment: On-GPU vs Off-GPU Bandwidth/memory_transfer_timer.cu
+++ Performance Experiment: On-GPU vs Off-GPU Bandwidth/memory_transfer_timer.cu
@@ -1,4 +1,3 @@
-%%cu
 #include <cstdio>
 
 // GPUs will vary on the max number of threads that can be in a block, but most can accomodate 512 per block
@@ -25,12 +24,12 @@ __global__ void knlMemCopy_1D(int *inbound, int *outbound, long size)
         outbound[uniqueID] = inbound[uniqueID];
 }
 
-float *kernelCopy(long transfer_size, int repetitions) {
+float *kernelCopy(long numElements, int repetitions) {
     // Calculate the number of blocks to use - we want one thread per element (byte) copied
-    int numBlocks =  transfer_size / THREADS_PER_BLOCK;
+    int numBlocks =  numElements / THREADS_PER_BLOCK;
 
     // If there are more bytes than can be evenly divided by a block's worth of threads, add a block to cover the rest
-    if (transfer_size % THREADS_PER_BLOCK > 0)
+    if (numElements % THREADS_PER_BLOCK > 0)
         numBlocks++;
 
     cudaEvent_t clockStart, clockStop;
@@ -38,21 +37,21 @@ float *kernelCopy(long transfer_size, int repetitions) {
     cudaEventCreate(&clockStop);
 
     int *d_input, *d_output;
-    cudaMalloc(&d_input, transfer_size*sizeof(int));
-    cudaMalloc(&d_output, transfer_size*sizeof(int));
+    cudaMalloc(&d_input, numElements*sizeof(int));
+    cudaMalloc(&d_output, numElements*sizeof(int));
     
 
     float trialTime;
     float *results = (float *) malloc(sizeof(float)*repetitions);
 
     // Initial run through to avoid any cold-start outliers
-    knlMemCopy_1D<<<numBlocks, THREADS_PER_BLOCK>>>(d_input, d_output, transfer_size);
+    knlMemCopy_1D<<<numBlocks, THREADS_PER_BLOCK>>>(d_input, d_output, numElements);
 
     for (int rep=0; rep < repetitions; rep++)
     {
         cudaEventRecord(clockStart, 0);
 
-            knlMemCopy_1D<<<numBlocks, THREADS_PER_BLOCK>>>(d_input, d_output, transfer_size);
+            knlMemCopy_1D<<<numBlocks, THREADS_PER_BLOCK>>>(d_input, d_output, numElements);
         
         cudaEventRecord(clockStop, 0);
         cudaEventSynchronize(clockStop);
@@ -67,12 +66,12 @@ float *kernelCopy(long transfer_size, int repetitions) {
     return results;
 }
 
-float *memcpyDtoD(long transfer_size, int repetitions) {
+float *memcpyDtoD(long numElements, int repetitions) {
 	// Calculate the number of blocks to use - we want one thread per element (byte) copied
-    int numBlocks =  transfer_size / THREADS_PER_BLOCK;
+    int numBlocks =  numElements / THREADS_PER_BLOCK;
 
     // If there are more bytes than can be evenly divided by a block's worth of threads, add a block to cover the rest
-    if (transfer_size % THREADS_PER_BLOCK > 0)
+    if (numElements % THREADS_PER_BLOCK > 0)
         numBlocks++;
 
     cudaEvent_t clockStart, clockStop;
@@ -80,21 +79,21 @@ float *memcpyDtoD(long transfer_size, int repetitions) {
     cudaEventCreate(&clockStop);
 
     int *d_input, *d_output;
-    cudaMalloc(&d_input, transfer_size*sizeof(int));
-    cudaMalloc(&d_output, transfer_size*sizeof(int));
+    cudaMalloc(&d_input, numElements*sizeof(int));
+    cudaMalloc(&d_output, numElements*sizeof(int));
     
 
     float trialTime;
     float *results = (float *) malloc(sizeof(float)*repetitions);
 
     // Initial run through to avoid any cold-start outliers
-    cudaMemcpy(d_output, d_input, transfer_size*sizeof(int), cudaMemcpyDeviceToDevice);
+    cudaMemcpy(d_output, d_input, numElements*sizeof(int), cudaMemcpyDeviceToDevice);
 
     for (int rep=0; rep < repetitions; rep++)
     {
         cudaEventRecord(clockStart, 0);
 
-            cudaMemcpy(d_output, d_input, transfer_size*sizeof(int), cudaMemcpyDeviceToDevice);
+            cudaMemcpy(d_output, d_input, numElements*sizeof(int), cudaMemcpyDeviceToDevice);
         
         cudaEventRecord(clockStop, 0);
         cudaEventSynchronize(clockStop);
@@ -109,12 +108,12 @@ float *memcpyDtoD(long transfer_size, int repetitions) {
     return results;
 }
 
-float *pagedDtoH(long transfer_size, int repetitions) {
+float *pagedDtoH(long numElements, int repetitions) {
 	// Calculate the number of blocks to use - we want one thread per element (byte) copied
-    int numBlocks =  transfer_size / THREADS_PER_BLOCK;
+    int numBlocks =  numElements / THREADS_PER_BLOCK;
 
     // If there are more bytes than can be evenly divided by a block's worth of threads, add a block to cover the rest
-    if (transfer_size % THREADS_PER_BLOCK > 0)
+    if (numElements % THREADS_PER_BLOCK > 0)
         numBlocks++;
 
     cudaEvent_t clockStart, clockStop;
@@ -122,21 +121,21 @@ float *pagedDtoH(long transfer_size, int repetitions) {
     cudaEventCreate(&clockStop);
 
     int *d_input, *h_output;
-    cudaMalloc(&d_input, transfer_size*sizeof(int));
-    h_output = (int *) malloc(transfer_size*sizeof(int));
+    cudaMalloc(&d_input, numElements*sizeof(int));
+    h_output = (int *) malloc(numElements*sizeof(int));
     
 
     float trialTime;
     float *results = (float *) malloc(sizeof(float)*repetitions);
 
     // Initial run through to avoid any cold-start outliers
-    cudaMemcpy(h_output, d_input, transfer_size*sizeof(int), cudaMemcpyDeviceToDevice);
+    cudaMemcpy(h_output, d_input, numElements*sizeof(int), cudaMemcpyDeviceToDevice);
 
     for (int rep=0; rep < repetitions; rep++)
     {
         cudaEventRecord(clockStart, 0);
 
-            cudaMemcpy(h_output, d_input, transfer_size*sizeof(int), cudaMemcpyDeviceToHost);
+            cudaMemcpy(h_output, d_input, numElements*sizeof(int), cudaMemcpyDeviceToHost);
         
         cudaEventRecord(clockStop, 0);
         cudaEventSynchronize(clockStop);
@@ -151,12 +150,12 @@ float *pagedDtoH(long transfer_size, int repetitions) {
     return results;
 }
 
-float *pinnedDtoH(long transfer_size, int repetitions) {
+float *pinnedDtoH(long numElements, int repetitions) {
 	// Calculate the number of blocks to use - we want one thread per element (byte) copied
-    int numBlocks =  transfer_size / THREADS_PER_BLOCK;
+    int numBlocks =  numElements / THREADS_PER_BLOCK;
 
     // If there are more bytes than can be evenly divided by a block's worth of threads, add a block to cover the rest
-    if (transfer_size % THREADS_PER_BLOCK > 0)
+    if (numElements % THREADS_PER_BLOCK > 0)
         numBlocks++;
 
     cudaEvent_t clockStart, clockStop;
@@ -164,21 +163,21 @@ float *pinnedDtoH(long transfer_size, int repetitions) {
     cudaEventCreate(&clockStop);
 
     int *d_input, *h_output;
-    cudaMalloc(&d_input, transfer_size*sizeof(int));
-    cudaMallocHost(&h_output, transfer_size*sizeof(int));
+    cudaMalloc(&d_input, numElements*sizeof(int));
+    cudaMallocHost(&h_output, numElements*sizeof(int));
     
 
     float trialTime;
     float *results = (float *) malloc(sizeof(float)*repetitions);
 
     // Initial run through to avoid any cold-start outliers
-    cudaMemcpy(h_output, d_input, transfer_size*sizeof(int), cudaMemcpyDeviceToDevice);
+    cudaMemcpy(h_output, d_input, numElements*sizeof(int), cudaMemcpyDeviceToDevice);
 
     for (int rep=0; rep < repetitions; rep++)
     {
         cudaEventRecord(clockStart, 0);
 
-            cudaMemcpy(h_output, d_input, transfer_size*sizeof(int), cudaMemcpyDeviceToHost);
+            cudaMemcpy(h_output, d_input, numElements*sizeof(int), cudaMemcpyDeviceToHost);
         
         cudaEventRecord(clockStop, 0);
         cudaEventSynchronize(clockStop);
@@ -193,12 +192,12 @@ float *pinnedDtoH(long transfer_size, int repetitions) {
     return results;
 }
 
-float *unifiedMemcpy(long transfer_size, int repetitions) {
+float *unifiedMemcpy(long numElements, int repetitions) {
 	// Calculate the number of blocks to use - we want one thread per element (byte) copied
-    int numBlocks =  transfer_size / THREADS_PER_BLOCK;
+    int numBlocks =  numElements / THREADS_PER_BLOCK;
 
     // If there are more bytes than can be evenly divided by a block's worth of threads, add a block to cover the rest
-    if (transfer_size % THREADS_PER_BLOCK > 0)
+    if (numElements % THREADS_PER_BLOCK > 0)
         numBlocks++;
 
     cudaEvent_t clockStart, clockStop;
@@ -206,21 +205,21 @@ float *unifiedMemcpy(long transfer_size, int repetitions) {
     cudaEventCreate(&clockStop);
 
     int *u_input, *u_output;
-    cudaMallocManaged(&u_input, transfer_size*sizeof(int));
-	cudaMallocManaged(&u_output, transfer_size*sizeof(int));
+    cudaMallocManaged(&u_input, numElements*sizeof(int));
+	cudaMallocManaged(&u_output, numElements*sizeof(int));
     
 
     float trialTime;
     float *results = (float *) malloc(sizeof(float)*repetitions);
 
     // Initial run through to avoid any cold-start outliers
-	memcpy(u_output, u_input, transfer_size*sizeof(int));
+	memcpy(u_output, u_input, numElements*sizeof(int));
 
     for (int rep=0; rep < repetitions; rep++)
     {
         cudaEventRecord(clockStart, 0);
 
-		memcpy(u_output, u_input, transfer_size*sizeof(int));
+		memcpy(u_output, u_input, numElements*sizeof(int));
         
         cudaEventRecord(clockStop, 0);
         cudaEventSynchronize(clockStop);
@@ -247,8 +246,8 @@ int main(int argc, char *argv[])
 
     const int MI_B = 1048576;    // One MiB or 2^20 bytes
 
-    // Total number of bytes (integer elements) to copy
-    long transfer_size = num_mibibytes * MI_B / sizeof(int);
+    // Determine number of integer elements for each transfer based on desired memory transfer amount
+    long numElements = num_mibibytes * MI_B / sizeof(int);
 
 	printf("type,size,unit,numAccesses");
     for (int rep=1; rep <= repetitions; rep++)
@@ -256,7 +255,7 @@ int main(int argc, char *argv[])
     printf("\n");
 
     // Test #1: GPU kernel copy capablilty    
-    float *kernelCopyTimes = kernelCopy(transfer_size, repetitions);
+    float *kernelCopyTimes = kernelCopy(numElements, repetitions);
 
 	printf("kernelCopy,%d,MB,2",num_mibibytes);
     for (int rep=0; rep < repetitions; rep++)
@@ -266,7 +265,7 @@ int main(int argc, char *argv[])
     free(kernelCopyTimes);
 
 	// Test #2: GPU-GPU memcpy capablilty    
-    float *deviceMemcpyTimes = memcpyDtoD(transfer_size, repetitions);
+    float *deviceMemcpyTimes = memcpyDtoD(numElements, repetitions);
 
 	printf("memcpyDtoD,%d,MB,2",num_mibibytes);
     for (int rep=0; rep < repetitions; rep++)
@@ -276,7 +275,7 @@ int main(int argc, char *argv[])
     free(deviceMemcpyTimes);
 
 	// Test #3: Paged GPU-CPU memcpy capablilty    
-    float *pagedMemcpyTimes = pagedDtoH(transfer_size, repetitions);
+    float *pagedMemcpyTimes = pagedDtoH(numElements, repetitions);
 
 	printf("pagedDtoH,%d,MB,2",num_mibibytes);
     for (int rep=0; rep < repetitions; rep++)
@@ -286,7 +285,7 @@ int main(int argc, char *argv[])
     free(pagedMemcpyTimes);
  
  	// Test #4: Pinned GPU-CPU memcpy capablilty    
-    float *pinnedMemcpyTimes = pinnedDtoH(transfer_size, repetitions);
+    float *pinnedMemcpyTimes = pinnedDtoH(numElements, repetitions);
 
 	printf("pinnedDtoH,%d,MB,2",num_mibibytes);
     for (int rep=0; rep < repetitions; rep++)
@@ -296,7 +295,7 @@ int main(int argc, char *argv[])
     free(pinnedMemcpyTimes);
 
 	// Test #5: Unified memory copy capablilty    
-    float *unifiedMemcpyTimes = unifiedMemcpy(transfer_size, repetitions);
+    float *unifiedMemcpyTimes = unifiedMemcpy(numElements, repetitions);
 
 	printf("unifiedMemcpy,%d,MB,2",num_mibibytes);
     for (int rep=0; rep < repetitions; rep++)
